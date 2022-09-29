7140317bd6817370a36b72f6ecd6e7cccac404cc
many bear <98581612+6666dustry@users.noreply.github.com>
2022-09-29 20:52:51 +0900

    new Item: darkmatter

--- src/Starner/content/StarnerItems.java
+++ src/Starner/content/StarnerItems.java
@@ -4,7 +4,7 @@ import arc.graphics.Color;
 import mindustry.type.Item;
 
 public class StarnerItems {
-    public static Item MoonStone, CometPiece, SunCrystal;
+    public static Item MoonStone, CometPiece, SunCrystal, DarkMatter;
 
     public static void load() {
         MoonStone = new Item("moon-stone", Color.valueOf("AAAAAA")) {
@@ -27,6 +27,16 @@ public class StarnerItems {
                 description = "sun crystal. very hot!";
                 cost = 0.75f;
                 flammability = 1f;
+            }
+        };
+        DarkMatter = new Item("sun-crystal", Color.black) {
+            {
+                description = "dark matter. what is this?";
+                cost = 1f;
+                flammability = 1f;
+                explosiveness = 1f;
+                radioactivity = 1f;
+                charge = 1f;
             }
         };
     }
